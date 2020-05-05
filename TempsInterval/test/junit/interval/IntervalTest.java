81b9d0d13399d44ef3bb7d59772154f0e27255cc
toniicalvn <garcia.calvin.toni@gmail.com>
2020-05-05 14:43:54 +0200

    estructuraProjecte

---
+++ TempsInterval/test/junit/interval/IntervalTest.java
@@ -0,0 +1,14 @@
+package junit.interval;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IntervalTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
